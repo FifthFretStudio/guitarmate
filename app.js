15d96497a4666cd82281cf70440afd7d6ce1ed03
fifthfretstudio <fifthfretstudio@gmail.com>
2025-05-16 21:59:31 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// GuitarMate Website JavaScript
+console.log('GuitarMate website loaded');
+
+// Basic interactivity can be added here later
+document.addEventListener('DOMContentLoaded', function() {
+    // Future interactive elements can be initialized here
+});
